248406c608c18553a9ea6c3fe8fe7596d62cd8d2
Younes Arroubi <yarroubi@e1r5p1.1337.ma>
2022-05-23 20:19:12 +0100

    socket in process

--- sockets/main.cpp
+++ sockets/main.cpp
@@ -2,36 +2,64 @@
 #include <netinet/in.h>
 
 #include <iostream>
+#include <unistd.h>
 
-int main(int argc, char **argv)
+typedef struct sockaddr t_sockaddr;
+typedef struct sockaddr_in t_sockaddr_in;
+
+const int bufferSize = 1024;
+
+struct sckt
 {
-	int	ret;
-	int server_fd;
-	struct sockaddr_in adress;
+	int				fd;
+	char			buffer[bufferSize];
+	socklen_t		addressLen;
+	t_sockaddr_in	adress;
+};
 
+int main()
+{
+	int		ret;
+	sckt	cnctSocket;
+	sckt	mainSocket;
 
-	if (argc != 4)
-		return 1;
-	server_fd = socket(PF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
+	mainSocket.fd  = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (mainSocket.fd < 0)
 	{
 		std::cerr << "cannot create socket !!!" << std::endl;
 		return 1;
 	}
-	adress.sin_family = AF_INET;
-	adress.sin_port = 8080;
-	adress.sin_addr.s_addr = htonl(INADDR_ANY);
-	ret = bind(server_fd, reinterpret_cast<struct sockaddr *>(&adress), sizeof(sockaddr_in));
+	mainSocket.adress.sin_family = AF_INET;
+	mainSocket.adress.sin_port = 8080;
+	mainSocket.adress.sin_addr.s_addr = htonl(INADDR_ANY);
+	ret = bind(mainSocket.fd, reinterpret_cast<t_sockaddr *>(&mainSocket.adress), sizeof(t_sockaddr_in));
 	if (ret < 0)
 	{
 		std::cerr << "cannot bind socket !!!" << std::endl;
 		return 1;
 	}
-	ret = listen(server_fd, 1000);
+	ret = listen(mainSocket.fd, 1000);
 	if (ret < 0)
 	{
 		std::cerr << "socket listening failed!!!" << std::endl;
 		return 1;
 	}
-
+	cnctSocket.fd = accept(mainSocket.fd, reinterpret_cast<t_sockaddr *>(&cnctSocket.adress), &cnctSocket.addressLen);
+	if (cnctSocket.fd < 0)
+	{
+		std::cerr << "accept failed!!!" << std::endl;
+		close(mainSocket.fd);
+		return 1;
+	}
+	ret = read(cnctSocket.fd, cnctSocket.buffer, bufferSize);
+	close(mainSocket.fd);
+	close(cnctSocket.fd);
+	if (ret < 0)
+	{
+		std::cerr << "read failed!!!" << std::endl;
+		return 1;
+	}
+	write(1, cnctSocket.buffer, ret);
+	std::cout << "server is shut down" << std::endl;
+	return 0;
 }
