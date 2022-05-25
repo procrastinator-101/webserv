a38f852018c3995863f4927920e992fb43e72c12
Younes Arroubi <yarroubi@e1r5p2.1337.ma>
2022-05-25 20:24:24 +0100

    server up and running

--- sockets/main.cpp
+++ sockets/main.cpp
@@ -1,65 +1,125 @@
-#include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 #include <iostream>
+#include <iomanip>
 #include <unistd.h>
 
-typedef struct sockaddr t_sockaddr;
-typedef struct sockaddr_in t_sockaddr_in;
+//colors
+#define BLACK "\x1B[0m"
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define YELLOW "\x1B[33m"
+#define CYAN "\x1B[37m"
+
+#define FIELDSIZE 30
 
 const int bufferSize = 1024;
+const int backLog = 10;
 
-struct sckt
+struct	Client
 {
-	int				fd;
-	char			buffer[bufferSize];
-	socklen_t		addressLen;
-	t_sockaddr_in	adress;
+	int			fd;
+	addrinfo	address;
+	char		buffer[bufferSize];
 };
 
-int main()
+int main(int argc, char **argv)
 {
-	int		ret;
-	sckt	cnctSocket;
-	sckt	mainSocket;
 
-	mainSocket.fd  = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (mainSocket.fd < 0)
+	// std::cout << GREEN << "hello" << std::endl;
+	int			fd;
+	int			ret;
+	addrinfo	hints;
+	addrinfo	server;
+	addrinfo	*result;
+
+	//setting the hints for the listening socket
+	bzero(&hints, sizeof(addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	// hints.ai_flags = AI_PASSIVE;
+
+	ret = getaddrinfo("127.0.0.1", "8080", &hints, &result);
+	if (ret || !result)
 	{
-		std::cerr << "cannot create socket !!!" << std::endl;
+		std::cout << RED << std::setw(FIELDSIZE) << "address creation" << "failure" << std::endl;
 		return 1;
 	}
-	mainSocket.adress.sin_family = AF_INET;
-	mainSocket.adress.sin_port = 8080;
-	mainSocket.adress.sin_addr.s_addr = htonl(INADDR_ANY);
-	ret = bind(mainSocket.fd, reinterpret_cast<t_sockaddr *>(&mainSocket.adress), sizeof(t_sockaddr_in));
-	if (ret < 0)
+	std::cout << std::left;
+	std::cout << GREEN << std::setw(FIELDSIZE) << "address creation" << "success" << std::endl;
+	server = *result;
+	freeaddrinfo(result);
+
+	//create a socker for listening
+	fd = socket(server.ai_family, server.ai_socktype, server.ai_protocol);
+	if (fd < 0)
 	{
-		std::cerr << "cannot bind socket !!!" << std::endl;
+		std::cout << RED << std::setw(FIELDSIZE) << "socket creation" << "failure" << std::endl;
 		return 1;
 	}
-	ret = listen(mainSocket.fd, 1000);
-	if (ret < 0)
+	std::cout << GREEN << std::setw(FIELDSIZE) << "socket creation" << "success" << std::endl;
+
+	//bind the server socket to an addres:port
+	ret = bind(fd, server.ai_addr, server.ai_addrlen);
+	if (ret)
 	{
-		std::cerr << "socket listening failed!!!" << std::endl;
+		std::cout << RED << std::setw(FIELDSIZE) << "socket binding" << "failure" << std::endl;
+		close(fd);
 		return 1;
 	}
-	cnctSocket.fd = accept(mainSocket.fd, reinterpret_cast<t_sockaddr *>(&cnctSocket.adress), &cnctSocket.addressLen);
-	if (cnctSocket.fd < 0)
+	std::cout << GREEN << std::setw(FIELDSIZE) << "socket binding" << "success" << std::endl;
+	//listen on the server socket
+	ret = listen(fd, backLog);
+	if (ret)
 	{
-		std::cerr << "accept failed!!!" << std::endl;
-		close(mainSocket.fd);
+		std::cout << RED << std::setw(FIELDSIZE) << "socket listening" << "failure" << std::endl;
+		close(fd);
 		return 1;
 	}
-	ret = read(cnctSocket.fd, cnctSocket.buffer, bufferSize);
-	close(mainSocket.fd);
-	close(cnctSocket.fd);
-	if (ret < 0)
+	std::cout << GREEN << std::setw(FIELDSIZE) << "socket listening" << "success" << std::endl;
+
+	//accept connections
+	while (1)
 	{
-		std::cerr << "read failed!!!" << std::endl;
-		return 1;
+		Client client;
+
+		client.address.ai_addrlen = sizeof(*client.address.ai_addr);
+		std::cout << YELLOW << std::setw(FIELDSIZE) << "listening" << "..." << std::endl;
+		client.fd = accept(fd, client.address.ai_addr, &client.address.ai_addrlen);
+		if (client.fd < 0)
+		{
+			std::cout << RED << std::setw(FIELDSIZE) << "connections acception" << "failure" << std::endl;
+			close(fd);
+			return 1;
+		}
+		std::cout << GREEN << "connect with " << client.fd << " : success" << std::endl;
+		ret = read(client.fd, client.buffer, bufferSize);
+		if (ret < 0)
+		{
+			std::cout << RED << std::setw(FIELDSIZE) << "reading from client" << "failure" << std::endl;
+			close(fd);
+			close(client.fd);
+			return 1;
+		}
+		std::cout << GREEN << "reading from " << client.fd << " : success" << std::endl;
+		std::string str(client.buffer, ret);
+		std::cout << BLACK;
+		std::cout << "==============================================================" << std::endl;
+		std::cout << str << std::endl;
+		std::cout << "==============================================================" << std::endl;
+		ret = write(client.fd, client.buffer, ret);
+		close(client.fd);
+		if (ret < 0)
+		{
+			std::cout << RED << std::setw(FIELDSIZE) << "writing to client" << "failure" << std::endl;
+			close(fd);
+			return 1;
+		}
+		std::cout << GREEN << "writing to " << client.fd << " : success" << std::endl;
 	}
-	write(1, cnctSocket.buffer, ret);
-	std::cout << "server is shut down" << std::endl;
 	return 0;
 }
---
+++ sockets/sample.cpp
@@ -0,0 +1,67 @@
+#include <cstdio>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <iostream>
+#include <unistd.h>
+
+const short int port = 8080;
+const int bufferSize = 1024;
+
+int main()
+{
+	int	ret;
+	int server, client;
+	sockaddr_in	address;
+	socklen_t	len = sizeof(address);
+	char buffer[bufferSize];
+
+	server = socket(AF_INET, SOCK_STREAM, 0);
+	if (server < 0)
+	{
+		std::cerr << "socket creation failed" << std::endl;
+		return 1;
+	}
+	std::cout << "socket creation : success , server :" << server << std::endl;
+	bzero(&address, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	address.sin_addr.s_addr = INADDR_ANY;
+	ret = bind(server, (sockaddr *)&address, sizeof(address));
+	if (ret < 0)
+	{
+		std::cerr << "socket binding failed" << std::endl;
+		close(server);
+		return 1;
+	}
+	std::cout << "socket binding : success" << std::endl;
+	ret = listen(server, 10);
+	if (ret < 0)
+	{
+		std::cerr << "socket listening failed" << std::endl;
+		close(server);
+		return 1;
+	}
+	std::cout << "listening ....." << std::endl;
+	client = accept(server, (sockaddr *)&address, &len);
+	std::cout << "DEDE" << std::endl;
+	if (client < 0)
+	{
+		std::cerr << "accept failed" << std::endl;
+		close(server);
+		return 1;
+	}
+	std::cout << "connection accepted" << std::endl;
+	ret = read(client, buffer, bufferSize);
+	close(server);
+	close(client);
+	if (ret < 0)
+	{
+		std::cerr << "read failed!!!" << std::endl;
+		return 1;
+	}
+	write(1, buffer, ret);
+	std::cout << "server is shut down" << std::endl;
+	return 0;
+}
