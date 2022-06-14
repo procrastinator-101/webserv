605c4b0348d45d82920c149b7cd91c0b673c223e
hoummadi <hamza.hoummadi.genie.info@gmail.com>
2022-06-14 20:12:58 +0100

    config 80% maybe 100%

--- srcs/location/Location.cpp
+++ srcs/location/Location.cpp
@@ -10,7 +10,145 @@ namespace ft
 	{
 	}
 
-	Location::Location(std::ifstream& configFile) : _root(NULL), _autoIndex(off), _methods(), _indexes(), _redirections(), _uploadPath(NULL)
+	void	Location::threat_root(std::stringstream& streamLine, std::string& token)
+	{
+		streamLine >> token;
+		if (token != "#")
+			_root = token;
+		else
+			throw std::runtime_error("Location: root is not valid");
+		if (streamLine.good())
+		{
+			streamLine >> token;
+			if (token != "#")
+				throw std::runtime_error("Location: too many arguments for root");
+		}
+	}
+
+	void	Location::threat_methods(std::stringstream& streamLine, std::string& token)
+	{
+		int size = _methods.size();
+		while (streamLine.good())
+		{
+			streamLine >> token;
+			if (token == "#")
+				break ;
+			if (token == "GET" || token == "POST" || token == "DELETE")
+				_methods.insert(token);
+			else
+				throw std::runtime_error("Location: method is not valid");
+		}
+		if (_methods.size() - size == 0)
+			throw std::runtime_error("Location: methods is not valid");
+	}
+
+	void	Location::threat_indexes(std::stringstream& streamLine, std::string& token)
+	{
+		int size = _indexes.size();
+		while (streamLine.good())
+		{
+			streamLine >> token;
+			if (token == "#")
+				break ;
+			_indexes.insert(token);
+		}
+		if (_indexes.size() - size == 0)
+			throw std::runtime_error("Location: indexes is not valid");
+	}
+
+	void	Location::threat_upload(std::stringstream& streamLine, std::string& token)
+	{
+		streamLine >> token;
+		if (token != "#")
+			_uploadPath = token;
+		else
+			throw std::runtime_error("Location: uploadPath is not valid");
+		if (streamLine.good())
+		{
+			streamLine >> token;
+			if (token != "#")
+				throw std::runtime_error("Location: too many arguments for uploadPath");
+		}
+	}
+
+	void	Location::threat_autoindex(std::stringstream& streamLine, std::string& token)
+	{
+		static int i = 0;
+
+		if (i == 1)
+			throw std::runtime_error("Location: multiple autoIndex");
+		streamLine >> token;
+		if (token == "on")
+			_autoIndex = true;
+		else if (token != "off")
+			throw std::runtime_error("Location: autoindex is not valid");
+		if (streamLine.good())
+		{
+			streamLine >> token;
+			if (token != "#")
+				throw std::runtime_error("Location: too many arguments for autoindex");
+		}
+		i = 1;
+	}
+
+	static	bool is_num(std::string &str)
+	{
+		for (size_t i = 0; i < str.size(); i++)
+		{
+			if (str[i] < '0' || str[i] > '9')
+				return false;
+		}
+		return true;
+	}
+
+	void	Location::threat_return(std::stringstream& streamLine, std::string& token)
+	{
+		std::string code_str;
+		lineStream >> code_str;
+		if (lineStream.good() && is_num(code_str))
+		{
+			int code = ::atoi(code_str.c_str());
+			try
+			{
+				HttpStatus::resolve(code);
+			}
+			catch (std::exception& e)
+			{
+				throw std::runtime_error("Location: return: invalid code");
+			}
+			lineStream >> token;
+			_redirection = std::make_pair(code, token);
+		}
+		else
+			throw std::runtime_error("Location: invalid code/path");
+		if (lineStream.good())
+		{
+			lineStream >> token;
+			if (token != "#")
+				throw std::runtime_error("Location: too many arguments for return");
+		}
+	}
+	
+
+
+
+	void	Location::threat_line(std::stringstream& streamLine, std::string& token)
+	{
+		if (token == "root" && streamLine.good())
+			threat_root(streamLine, token);
+		else if (token == "methods" && streamLine.good())
+			threat_methods(streamLine, token);
+		else if (token == "index" && streamLine.good())
+			threat_index(streamLine, token);
+		else if (token == "upload" && streamLine.good())
+			threat_upload(streamLine, token);
+		else if (token == "autoindex" && streamLine.good())
+			threat_autoindexd(streamLine, token);
+		else if (token == "return" && streamLine.good())
+			threat_return(streamLine, token);
+	}
+
+	Location::Location(std::ifstream& configFile, std::string& root, status autoIndex, std::set<std::string>& indexes, std::set<std::string>& methods) : _root(NULL), _autoIndex(false), _methods(), _indexes(), _redirections(), _uploadPath(NULL)
 	{
 		int				inside_location = 0;
 		std::string		line;
@@ -37,11 +175,30 @@ namespace ft
 				inside_location = 0;
 				break ;
 			}
-
-			
+			threat_line(streamLine, token);
 		}
 		if (inside_location == 1)
 			throw std::runtime_error("Location: config file is not valid");
+
+		// take server data
+		if (_root == NULL)
+			_root = root;
+		if (_autoIndex == false)
+			_autoIndex = autoIndex;
+		if (_indexes.empty())
+		{
+			if (indexes.empty() && _autoIndex == false) 
+				throw std::runtime_error("Location: config file is not valid");
+			else if (!indexes.empty())
+				_indexes = indexes;
+		}
+		if (_methods.empty())
+		{
+			if (methods.empty())
+				throw std::runtime_error("Location: config file is not valid");
+			else
+				_methods = methods;
+		}
 	}
 
 	Location::Location(const Location& src) :	_root(src._root), _autoIndex(src._autoIndex), _methods(src._methods), _indexes(src._indexes),
--- srcs/location/Location.hpp
+++ srcs/location/Location.hpp
@@ -21,17 +21,25 @@ namespace ft
 			status						_autoIndex;
 			std::set<std::string>		_methods;
 			std::set<std::string>		_indexes;
-			std::map<int, std::string>	_redirections;
+			std::pair<int, std::string>	_redirection;
 			std::string					_uploadPath;
 
 			void	_deepCopy(const Location& src);
+
+			void	threat_line(std::stringstream& streamLine, std::string& token);
+			void	threat_root(std::stringstream& streamLine, std::string& token);
+			void	threat_methods(std::stringstream& streamLine, std::string& token);
+			void	threat_indexes(std::stringstream& streamLine, std::string& token);
+			void	threat_upload(std::stringstream& streamLine, std::string& token);
+			void	threat_autoindex(std::stringstream& streamLine, std::string& token);
+			void	threat_return(std::stringstream& streamLine, std::string& token);
 		
 		public:
 			Location();
 			~Location();
 
 			//might throw
-			Location(std::ifstream& configFile);
+			Location(std::ifstream& configFile, std::string& root, status autoIndex, std::set<std::string>& indexes, std::set<std::string>& methods);
 			Location(const Location& src);
 			
 			Location	&operator=(const Location& rop);
